51e12c29f013ff60bbd9a13b3c667318ade8cc50
IDontWannaPlay <erictai9912@gmail.com>
2023-12-28 11:20:49 -0800

    SSIM Test

--- StrokeDetection/SSIMTest.py
+++ StrokeDetection/SSIMTest.py
@@ -4,8 +4,8 @@ from skimage.metrics import structural_similarity
 import glob
 
 # Load images
-before = cv2.imread('test_images/ssim_test/before.jpeg')
-after = cv2.imread('test_images/ssim_test/after.jpeg')
+before = cv2.imread('test_images/ssim_test/1_before.jpeg')
+after = cv2.imread('test_images/ssim_test/1_after.jpeg')
 
 # # Convert images to grayscale
 # before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
@@ -21,4 +21,27 @@ print("Image Similarity: {:.4f}%".format(score * 100))
 
 # diff is [0,1], scale to [0,255]
 diff = (diff * 255).astype("uint8")
-diff_box = cv2.merge([diff, diff, diff])
+diff_gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+diff_blur = cv2.GaussianBlur(diff_gray,(5,5),0)
+
+# cv2.imshow('diff', diff_blur)
+
+ret, bw = cv2.threshold(diff_gray,200,255,cv2.THRESH_BINARY) #+cv2.THRESH_OTSU
+contours, _ = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+for i, c in enumerate(contours):
+  area = cv2.contourArea(c)
+
+  if area < 100 or area > 100000:
+    continue
+
+  rect = cv2.minAreaRect(c)
+  box = cv2.boxPoints(rect)
+  box = np.intp(box)
+  cv2.drawContours(before, [box], 0, (0,0,255), 2)
+  cv2.imshow('Before Image', before)
+  cv2.drawContours(after, [box], 0, (0,0,255), 2)
+  cv2.imshow('After Image', after)
+
+# cv2.imshow('Binarized Image', bw)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
